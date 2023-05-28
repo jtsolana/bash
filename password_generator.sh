277637d0505750f5f156e1bbd33b67ee7d9bc4e8
jtsolana <jtsolana@gmail.com>
2023-05-28 14:45:00 +0800

    Added another function that generates password with at least one special character

--- password_generator.sh
+++ password_generator.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
-# A simple password generator that display 8 character or more.
+# This Script Generate a random password with 8 character and at least one special character.
 # Simply modify the head -c8 to generate the desired number of characters.
 
-PASSWORD=$(date +%s%N | sha256sum | head -c8)
-echo "${PASSWORD}"
+# Define an array of special characters
+special_chars=("!" "#" "$" "%" "&" "@")
+
+# Generate a random password
+password=$(date +%s%N | sha256sum | head -c8)
+
+# Choose a random special character from the array
+random_char=${special_chars[$((RANDOM % ${#special_chars[@]}))]}
+
+# Insert the random special character into the password
+password="${password:0:1}${random_char}${password:1}"
+
+echo $password
